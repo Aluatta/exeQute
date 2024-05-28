54921cb891a34572f656070eade3e59d1cf346c0
Aluatta <113020742+Aluatta@users.noreply.github.com>
2024-05-28 07:38:24 +0200

    added javascript for mobile menu
    fixed alignment , added target blank to logo,   added  constant propoperties

--- javascript.js
+++ javascript.js
@@ -14,3 +14,15 @@ function closeMenu() {
     hamburger.classList.remove('active');
     navMenu.classList.remove('active');
 }
+/** Added event listener  to make an hover effect on meni items for mobiles  and tablets */
+document.addEventListener('touchstart', function(event) {
+    if (event.target.classList.contains('nav-link')) {
+        event.target.classList.add('hover');
+    }
+});
+
+document.addEventListener('touchend', function(event) {
+    if (event.target.classList.contains('nav-link')) {
+        event.target.classList.remove('hover');
+    }
+});
